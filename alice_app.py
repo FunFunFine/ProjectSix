126cf486f725c2ad0749bab08673126df8e88f5f
Ratbek Nurlanov <ratbeknurlanov@gmail.com>
2018-07-28 10:59:49 +0300

    Project has been launched

---
+++ alice_app.py
@@ -0,0 +1,35 @@
+from __future__ import unicode_literals
+
+import json
+import logging
+
+from alice_sdk import AliceRequest, AliceResponse
+
+from flask import Flask, request
+app = Flask(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
+
+session_storage = {}
+@app.route("/", methods=['POST'])
+def main():
+    alice_request = AliceRequest(request.json)
+    logging.info('Request: {}'.format(alice_request))
+
+    alice_response = AliceResponse(alice_request)
+
+    user_id = alice_request.user_id
+
+    alice_response, session_storage[user_id] = handle_dialog(
+        alice_request, alice_response, session_storage.get(user_id)
+    )
+
+    logging.info('Response: {}'.format(alice_response))
+
+    return alice_response.dumps()
+
+
+def handle_dialog(request, response, user_starage):
+    response.set_text('Иди на хуй')
+
+    return response, user_starage
