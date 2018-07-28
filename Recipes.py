d07bc43e5912fae025d60de42653eaf76d435c8f
FunFunFine <31823086+FunFunFine@users.noreply.github.com>
2018-07-28 16:34:15 +0500

    Update Recipes.py

--- Recipes.py
+++ Recipes.py
@@ -1,9 +1,17 @@
 from requests_html import *
 import json
-with ('ingridients.json','r') as f:
+
+with ('./ingridients.json', 'r') as f:
     INGRIDIENTS = json.load(f)
 
 
+def get_recipes(ingridients, amount=1):
+    try:
+        nums = words_to_digits(ingridients)
+        links = get_links(*nums)
+        return links[:amount]
+    except Exception as e:
+        print(e)
 
 
 def extract(page):
@@ -36,6 +44,21 @@ def words_to_digits(ingredients):
     return ingred
 
 
+def get_links(*ingr_nums):
+    init_link = 'https://eda.ru/recepty/ingredienty/' + '/'.join(ingr_nums)
+    session = HTMLSession()
+    r = session.get(init_link)
+    dict1 = r.html.find('div,p,div,ul,div')
+    links = []
+    names = []
+    for e in dict1[1:]:
+        if 'data-title' in e.attrs:
+            links.append(e.attrs['data-title'])
+        if 'data-href' in e.attrs:
+            s = e.attrs['data-href']
+            if s.startswith('/'):
+                names.append('https://eda.ru/' + s)
+    return list(iter(zip(links, names)))
 
-print(words_to_digits(['рис', 'сыр']))
 
+print(get_links(words_to_digits(['рис', 'сыр'])))
